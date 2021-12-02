38a5b219d8717926f705b6094c1b8162c213e70c
Zach Lott <zacharylott94@gmail.com>
2021-12-02 16:43:49 -0600

    day 2 part 1 complete

--- day2/Program.fs
+++ day2/Program.fs
@@ -1,13 +1,39 @@
 // Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
 
-open System
+open System.IO
 
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+type Command =
+    | Forward of int
+    | Up of int
+    | Down of int
+
+type Position = int * int
+
+
+let parse (str:string) =
+    str.Split()
+    |> fun x -> (x.[0], x.[1])
+    |> fun (command, value) -> 
+        match command with
+        | "down" -> Down (int value)
+        | "up" -> Up (int value)
+        | "forward" -> Forward (int value)
+        | _ -> failwith "Invalid command"
+
+let doCommand (h, d) command =
+    match command with
+    | Up x -> (h, d - x)
+    | Down x -> (h, d + x)
+    | Forward x -> (h + x, d)
 
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
+    let lines = File.ReadAllLines("input")
+    lines
+    |> Array.map parse
+    |> Array.fold (doCommand) (0,0)
+    // |> fun (h, d) -> h * d
+    |> fun (h,d) -> printfn "Horizontal:%d * Depth:%d = %d" h d (h*d)
+    
+
     0 // return an integer exit code
