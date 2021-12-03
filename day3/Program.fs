8bb0d50bfe4e585626bfb8747d910e983851d5fe
Zach Lott <zacharylott94@gmail.com>
2021-12-03 08:29:20 -0600

    day 3 part 1 complete

--- day3/Program.fs
+++ day3/Program.fs
@@ -1,13 +1,58 @@
 // Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
 
+open System.IO
 open System
+open FSharp.Core
 
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+type Bit = 
+    |One 
+    |Zero
+type BinaryNumber = Bit[]
+
+let parse (str:string) : BinaryNumber =
+    str.ToCharArray()
+    |> Array.map (function
+        | '1' -> One
+        | '0' -> Zero
+        | _ -> failwith "Not 1 or 0"
+            )
+let decideBit (bits:Bit[]): Bit =
+    bits
+    |> Array.fold (fun (ones, zeros) bit -> 
+        match bit with
+        | One -> (ones + 1, zeros)
+        | Zero -> (ones, zeros + 1)
+        ) (0,0)
+    |> fun (ones, zeros) -> if (ones > zeros) then One else Zero     
+ 
+let parseStringToNumber (str:string) =
+    str.ToCharArray()
+    |> Array.rev
+    |> Array.indexed
+    |> Array.map (fun (i,b) -> ((string >> int) b)<<<i)
+    |> Array.sum
+
+let flipBits (bits:Bit[]): Bit[] =
+    Array.map (function
+        | One -> Zero
+        | Zero -> One) bits
+let bitsToString bits =
+    Array.fold (fun str bit ->
+        match bit with
+        | One -> str+"1"
+        | Zero -> str+"0"
+    ) ""  bits  
 
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
+    File.ReadAllLines("input")
+    |> Array.map parse
+    |> Array.transpose
+    |> Array.map decideBit
+    |> fun x -> (x, flipBits x)
+    |> fun (x,y) -> Array.map bitsToString [|x;y|]
+    |> Array.map parseStringToNumber
+    |> Array.reduce (*)
+    |> printfn "final result: %d"
+
     0 // return an integer exit code
