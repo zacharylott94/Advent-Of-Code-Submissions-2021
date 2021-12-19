900b716a24f14b555ee44b54749455da8eed710d
Zach Lott <zacharylott94@gmail.com>
2021-12-19 13:41:48 -0600

    day4 restart

--- day4/Array2D.fs
+++
@@ -1,7 +0,0 @@
-module Array2d
-let fold2 (folder:'U -> 'T -> 'U) (state:'U) (arr2d:'T[,]) : 'U =
-  let mutable result = state
-  Array2D.iter (fun t -> (result <- (folder result t)) ) arr2d
-
-  result
-
---
+++ day4/task1/Library.fs
@@ -0,0 +1,5 @@
+﻿namespace task1
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
